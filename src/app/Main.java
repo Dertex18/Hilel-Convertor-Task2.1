798a09ec3497485db5562d23216ab33b934c004e
Mykyta Firsov <nik.firs1070@gmail.com>
2026-04-06 23:22:57 +0300

    Add app info

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,7 +3,8 @@ package app;
 public class Main {
     public static void main(String[] args) {
 
-            System.out.println("Test push with bash");
+            System.out.println("App for distant convertion");
+            System.out.println("Version 1.0");
 
 
     }
