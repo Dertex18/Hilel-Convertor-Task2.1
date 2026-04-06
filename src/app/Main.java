2c62faf4a337524d0d342b76d09c8ec839745a66
Mykyta Firsov <nik.firs1070@gmail.com>
2026-04-06 23:32:26 +0300

    add convert miles to km

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,11 +1,21 @@
 package app;
 
 public class Main {
+    private static final double km = 1.609344;
     public static void main(String[] args) {
 
             System.out.println("App for distant convertion");
             System.out.println("Version 1.0");
 
+            double miles = 12;
+
+            double resultInKm = convertMilesToKilometers(miles);
+            System.out.println("Result is " + resultInKm + " kilometers");
 
     }
+
+    private static double convertMilesToKilometers(double miles) {
+        return miles*km;
+    }
+
 }
