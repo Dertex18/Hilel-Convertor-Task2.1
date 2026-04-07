2ac0831c471be497e2026e71f3996f550b0e8d7c
Mykyta Firsov <nik.firs1070@gmail.com>
2026-04-07 12:24:08 +0300

    formatted the code.

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,26 +2,24 @@ package app;
 
 public class Main {
     private static final double km = 1.609344;
+
     public static void main(String[] args) {
 
-            System.out.println("App for distant convertion");
-            System.out.println("Version 1.0");
+        System.out.println("App for distant convertion");
+        System.out.println("Version 1.0");
 
-            double miles = 12;
-            double kilometers = 34;
-            double resultInKm = convertMilesToKilometers(miles);
-            double resultInMiles = convertKilometersToMiles(kilometers);
-            System.out.println("Result is " + resultInKm + " kilometers and " + resultInMiles + " miles");
-
-
-
+        double miles = 12;
+        double kilometers = 34;
+        double resultInKm = convertMilesToKilometers(miles);
+        double resultInMiles = convertKilometersToMiles(kilometers);
+        System.out.println("Result is " + resultInKm + " kilometers and " + resultInMiles + " miles");
     }
 
     private static double convertMilesToKilometers(double miles) {
-        return miles*km;
+        return miles * km;
     }
 
     private static double convertKilometersToMiles(double kilometers) {
-        return kilometers/km;
+        return kilometers / km;
     }
 }
